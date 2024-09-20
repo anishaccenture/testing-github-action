734cbb348191dbd0844291179c807157e8c87c17
github-cloudlabsuser-664 <github_cloudlabsuser_664@cloudlabsaiuser.com>
2024-09-20 13:21:08 +0530

    openAI_implementation

--- function_app.py
+++ function_app.py
@@ -1,13 +1,18 @@
-import azure.functions as func
 import logging
+import azure.functions as func
+from openai import AzureOpenAI
+#from GenerateTextFunction import generate_text_on_topic
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+client = AzureOpenAI(
+            api_version = "2024-02-15-preview",
+            azure_endpoint = "https://demoazureai6064269374.openai.azure.com/",
+            api_key = "a2af6b0615c54002bb66474dccd832c6"
+            )
 
-@app.route(route="testfunction")
-def testfunction(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
+    name = req.params.get('name')  
     if not name:
         try:
             req_body = req.get_json()
@@ -17,9 +22,41 @@ def testfunction(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        Topic = generate_text_on_topic(name)
+        logging.info(Topic)
+        #print(Topic)
+        return func.HttpResponse(f"Let’s crack a joke about {name}!: {Topic}")
+        #return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully.anish and sohini Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a Topic to generate a jokes.",
              status_code=200
         )
+
+def generate_text_on_topic(topic: str) -> str:
+    # Ensure the OpenAI API key is set
+    #openai.api_key = "b00f1602be9f435fbc423597af147e21"
+    
+    # Define the prompt for the OpenAI model
+    prompt = f"Write a joke on this topic: {topic}"
+    
+    # Call the OpenAI API to generate text
+    #prompt = {resume_text}
+    messages = [
+            {"role": "system", "content": "You are a helpful assistant.how can generate a joke for you"},
+            {"role": "user", "content":f'{prompt}'}]
+    response = client.chat.completions.create(
+            model="gpt-4o-funcion",
+            messages = messages,
+            temperature=0.5,
+            max_tokens=400,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None)
+    # #text = response.choices[0].message.content
+    
+    # Extract the generated text from the response
+    generated_text = response.choices[0].message.content    
+    #generated_text = f"generate_text_on_topic > Generate a topic on {topic}"   
+    return generated_text
